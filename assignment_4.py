514a6f534fc369105ad14e6f124efe99312844fb
Idt Danny <idtdanny@gmail.com>
2025-06-12 20:26:30 +0200

    Day4 D

--- assignment_4.py
+++ assignment_4.py
@@ -7,13 +7,15 @@ elif average >= 80 and lgrade >= 70:
     print("Second Upper Class A")
 elif average >= 80 and lgrade <= 60:
     print("Second Lower Class A")
-elif average >= 70 and lgrade >= 70:
+elif average >= 70 or lgrade >= 70:
     print("Second Upper Class B")
-elif average >= 70 and lgrade < 60:
+elif average >= 70 or lgrade < 60:
     print("Second Lower Class B")
 elif average >= 60 and lgrade >= 50:
     print("Second Lower Class C")
 elif average >= 60 and lgrade <= 50:
-    print("Pass")
+    print("Pass A")
+elif average >= 50:
+    print("Pass B")
 else:
     print("Fail")
