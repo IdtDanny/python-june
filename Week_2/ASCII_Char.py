4e3a5fd1d40a306bb2495d937ce22241a4ee92ca
Danny I <idtdanny@gmail.com>
2025-06-17 12:13:37 +0200

    Added Ascii

---
+++ Week_2/ASCII_Char.py
@@ -0,0 +1,9 @@
+lowerCase = []
+for i in range(65, 65+25):
+    lowerCase.append(chr(i))
+print(lowerCase)
+
+upperCase = []
+for i in range(97, 97+25):
+    upperCase.append(chr(i))
+print(upperCase)
--- Week_2/ListPrime.py
+++ Week_2/ListPrime.py
@@ -1,3 +1,15 @@
 limit = int(input('Enter the limit to list: '))
 
-prime
+prime = []
+
+start = 2
+
+while start <= limit:
+    for i in range(2, start):
+        if start % i == 0:
+            break
+    else:
+        prime.append(start)
+    start += 1
+
+print(prime)
