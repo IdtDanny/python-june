ac3d35d4264c7acfadc874b7d6099f3911011e33
Idt Danny <idtdanny@gmail.com>
2025-06-20 18:20:02 +0200

    20 June - Part 1

--- Week_2/Triangle2.py
+++ Week_2/Triangle2.py
@@ -4,7 +4,8 @@ angleA = float(input('Enter angle A: '))
 angleB = float(input('Enter angle B: '))
 sideB = float(input('Enter side B: '))
 
-
+# Sine rule: b / sinB = a / sinA = c / sinC
+# Test with A = 37, B=53, C=90, a=3, b=4, c = 5, Area = 6
 
 sideA = sideB * math.sin(math.radians(angleA)) / math.sin(math.radians(angleB))
 
